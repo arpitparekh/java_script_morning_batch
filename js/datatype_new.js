95abca0affac9af3af1907a371a3e58313c5ca78
arpitparekh <arpitparekh9@gmail.com>
2024-07-05 04:24:58 +0530

    new_datatype

---
+++ js/datatype_new.js
@@ -0,0 +1,58 @@
+// array data type
+
+// number
+// 12 12.23
+let a = 12
+let b = 123.4
+
+// string
+
+let name = "Bascom"
+
+// boolean
+
+let isLogin = false
+
+// array
+
+let values = [1, 2, 3, 54, 6, 5, 7, 56, 56, 33]  // many values under single name
+
+console.log(values)
+console.log(values[3])
+
+values[3] = 45
+console.log(values)
+
+// empty array
+
+let data = []
+
+data[0] = "Bascom"
+data[1] = "Jimit"
+data[2] = "Holy Water"
+
+console.log(data)
+
+console.log(data.length)
+
+let funtoos = [12, 45, 67, "bascom", true, "arpit", 45, 45]  // multi type array
+
+console.log(funtoos)
+
+let home = [123, 56, 34, 67, 8, 5, 3, 2, 6, 657, 657, 3435];
+
+// object datatype  // key-values pair
+
+let person = {
+    'name': 'arpit',
+    'address': 'vastrapur',
+    'age': 27,
+    'name': "maulik",
+    'name':"bascom"
+}
+
+console.log(person)
+
+console.log(person['address'])
+
+console.log(person.name)
